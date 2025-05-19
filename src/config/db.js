f3937b211b13155ec57a43840fdf2685bf679ef1
maimasaamar <maimas.aamar@e.braude.ac.il>
2025-05-19 12:04:12 +0300

    update1

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+// רק אם את עובדת עם ESM (import/export)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// נתיב מוחלט אל קובץ .env בתיקיית הפרויקט הראשית
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
 
 export const connectDB = async () => {
   try {
@@ -10,9 +18,9 @@ export const connectDB = async () => {
       .replace('<PASSWORD>', process.env.MONGO_PASSWORD);
 
     const conn = await mongoose.connect(uri);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
+    console.error(` MongoDB Connection Error: ${error.message}`);
     throw error;
   }
 };
