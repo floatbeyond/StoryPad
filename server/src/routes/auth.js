06752df7038523c8cbdd5942d987fc65239f2e40
Alon Dahan <alon.ed@outlook.com>
2025-06-11 17:36:18 +0300

    imported the wrong bcrypt (bcryptjs to bcrypt)

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/User.js';
 import { SECRET_KEY } from '../middleware/auth.js';
