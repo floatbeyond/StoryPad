5451f0a9e24e26cdd604bda4dd0123aa93df429b
Alon Dahan <alon.ed@outlook.com>
2025-06-11 17:32:26 +0300

    added missing bcrypt import in auth

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/User.js';
 import { SECRET_KEY } from '../middleware/auth.js';
